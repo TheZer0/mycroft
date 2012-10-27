55e9687f4158be534ccc921deab5a8b41b7a13a1
Nick Taylor <thezer0@lavabit.com>
2012-10-27 15:00:18 -0600

    Removing build files

--- src/modules/ThreatLevel.h
+++
@@ -1,6 +0,0 @@
-#ifndef THREAT_LEVEL_H
-#define THREAT_LEVEL_H
-
-char* getThreatLevel();
-
-#endif
--- src/mycroft/mycroft.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "ThreatLevel.h"
-
-int main()
-{
-    printf("Welcome back commander.\n");
-    printf("The current threat level is: %s\n", getThreatLevel());
-    return 0;
-}
